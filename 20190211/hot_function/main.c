71329b27ec4d7e932f0f07f778aa82f579b11f3b
alphabetc1 <2508695655@qq.com>
2019-02-15 17:16:50 +0800

    http&crypt&hot_function&md5

---
+++ 20190211/hot_function/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define N 1000000
+static inline void cal()
+{
+	int i=0;
+	for(i=0;i<100;i++)
+	{
+	}
+}
+int main()
+{
+	int i;
+	printf("I am printf\n");
+	for(i=0;i<N;i++)
+	{
+		cal();
+	}
+}
